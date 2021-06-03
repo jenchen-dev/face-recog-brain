e44c7232eb792ee87fe7a291c13d234fef56e26e
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-06-03 23:56:58 +0800

    adding new features and styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-const Navigation = () => {
-    return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <div className='pa4'>
-                <button className='f5 link dim br-pill b--blue ph3 pv2 mb2 dib white bg-dark-blue'>Sign Out</button>
-            </div>
-        </nav>
-    );
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+        if (isSignedIn) {
+            return (
+                <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <div className='pa4'>
+                        <button onClick={() => onRouteChange('signout')} className='ma1 f5 link dim br-pill b--blue ph3 pv2 mb2 dib white bg-dark-blue'>Sign Out</button>
+                    </div>
+                </nav>
+            );
+        } else {
+            return (
+                <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <div className='pa4'>
+                        <button onClick={() => onRouteChange('signin')} className='ma1 f5 link dim br-pill b--blue ph3 pv2 mb2 dib white bg-dark-blue'>Sign In</button>
+                        <button onClick={() => onRouteChange('register')} className='ma1 f5 link dim br-pill b--blue ph3 pv2 mb2 dib white bg-dark-blue'>Register</button>
+                    </div>
+                </nav>
+            );
+        }
 }
 
 export default Navigation;
