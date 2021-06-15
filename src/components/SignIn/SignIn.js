e5a0250426e65d03dd53ef03c6816c51fcf8046c
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-06-15 19:27:28 +0800

    deploy

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://protected-castle-51424.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
